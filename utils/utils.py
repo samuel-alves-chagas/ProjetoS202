38fe2d8fe38bcf8c74ed746c14c687de33d2cbd8
Samuel <samuel@DESKTOP-FTJR4R0.localdomain>
2022-06-14 23:21:44 -0300

    Finishing v1

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,5 @@
+import random
+
 inputs = {
     1: '1. Para listar todos os pilotos já cadastrados no sistema de rachas',
     2: '2. Para listar todos os carros já cadastrados',
@@ -21,3 +23,12 @@ def generateOptionStr():
 
 def divider():
     print('\n', 80*'_', '\n')
+
+
+def probabilidadeDeVencer(piloto, carro):
+    calculoBase = piloto['habilidade'] * carro['cavalos']
+    calculoComTurbo = calculoBase * 1.25 if carro['turbo'] else calculoBase
+    if piloto['calvo'] and random.randint(0, 10) > 8:
+        return calculoComTurbo * 2
+    else:
+        return calculoComTurbo
--- view/cli_interface.py
+++ view/cli_interface.py
@@ -1,7 +1,7 @@
 from pprintpp import pprint as pp
 from model.motoristaDAO import MotoristaDAO
 from model.carroDAO import CarroDAO
-from utils.utils import divider, generateOptionStr
+from utils.utils import divider, generateOptionStr, probabilidadeDeVencer
 
 
 def view(connection):
@@ -118,68 +118,147 @@ def view(connection):
 
             motoristaDAO.update_baldness(motorista)
             print('Agora o piloto', nome, 'está mais aerodinâmico!!')
-            # pp(aux)
-
-            # carro = {
-            #     'modelo': 'Corvette',
-            #     'turbo': True
-            # }
-
-            # aux = carroDAO.update_turbo(carro)
-            # pp(aux)
-
             divider()
 
-            # aux = motoristaDAO.read_all()
-            # pp(aux)
-            # aux = carroDAO.read_all()
-            # pp(aux)
-            # divider()
+        elif option == '6':
+            print('Você escolheu tunar um dos carros')
 
-            # carro = {
-            #     'modelo': 'Corvette',
-            #     'cor': 'prata',
-            #     'cavalos': 217,
-            #     'turbo': False
-            # }
-            # carroDAO.create(carro)
+            results = carroDAO.read_all()
+            if len(results) > 0:
+                print('Esses são os carros disponíveis:')
+                data = []
+                for record in results:
+                    data.append(record['c']._properties['modelo'])
 
-        # elif option == '3':
-        #     motorista = {
-        #         'nome': 'Samuel',
-        #         'calvo': True
-        #     }
+                for record in data:
+                    pp(record)
 
-        #     aux = motoristaDAO.update_baldness(motorista)
-        #     pp(aux)
+            else:
+                print('Porém nenhum carro foi cadastrado até o momento')
 
-        #     carro = {
-        #         'modelo': 'Corvette',
-        #         'turbo': True
-        #     }
+            modelo = input(
+                'Qual deles deve ganhar um pouco mais de torque? ')
 
-        #     aux = carroDAO.update_turbo(carro)
-        #     pp(aux)
+            carro = {
+                'modelo': modelo,
+                'turbo': True
+            }
 
-        #     divider()
+            carroDAO.update_turbo(carro)
+            print('O carro', modelo, 'foi tunado')
+            divider()
 
-        # elif option == '4':
-        #     motorista = {
-        #         'nome': 'Samuel'
-        #     }
-        #     motoristaDAO.delete(motorista)
+        elif option == '7':
+            print('Você escolheu excluir um item do sistema')
 
-        #     carro = {
-        #         'modelo': 'Corvette'
-        #     }
-        #     carroDAO.delete(carro)
+            aux = input(
+                'Você deseja excluir um carro ou um piloto? (C=carro, P=piloto) ')
 
-        #     aux = motoristaDAO.read_all()
-        #     pp(aux)
-        #     aux = carroDAO.read_all()
-        #     pp(aux)
+            if aux == 'C':
+                results = carroDAO.read_all()
+                if len(results) > 0:
+                    print('Esses são os carros cadastrados:')
+                    data = []
+                    for record in results:
+                        data.append(record['c']._properties['modelo'])
 
-        #     divider()
+                    for record in data:
+                        pp(record)
 
+                    modelo = input('Qual deles deverá ser removido? ')
+                    carro = {
+                        'modelo': modelo
+                    }
+                    carroDAO.delete(carro)
+
+                    print('O modelo', modelo, 'foi removido')
+
+                else:
+                    print('Não há nenhum carro no sistema, cancelando operação')
+
+            elif aux == 'P':
+                results = motoristaDAO.read_all()
+                if len(results) > 0:
+                    print('Esses são os motoristas cadastrados:')
+                    data = []
+                    for record in results:
+                        data.append(record['m']._properties['nome'])
+
+                    for record in data:
+                        pp(record)
+
+                    nome = input('Qual deles deverá ser removido? ')
+                    motorista = {
+                        'nome': nome
+                    }
+                    motoristaDAO.delete(motorista)
+
+                    print('O motorista', motorista, 'foi removido do sistema')
+                else:
+                    print(
+                        'Não há nenhum piloto cadastrado no sistema, cancelando operação')
+            else:
+                print('Não foi selecionada uma opção válida, cancelando operação')
+            divider()
+
+        elif option == '8':
+            print('Você escolheu DISPUTAR UM RACHA')
+
+            motoristas = motoristaDAO.read_all()
+            carros = carroDAO.read_all()
+            if len(motoristas) > 0 and len(carros) > 0:
+                print('Esses são os motoristas cadastrados:')
+                data = []
+                for record in motoristas:
+                    data.append(record['m']._properties['nome'])
+
+                for record in data:
+                    pp(record)
+
+                piloto1 = input('Qual deles deverá ser o piloto n° 1? ')
+                for record in motoristas:
+                    if record['m']._properties['nome'] == piloto1:
+                        piloto1 = record['m']._properties
+
+                piloto2 = input('E o piloto n° 2? ')
+                for record in motoristas:
+                    if record['m']._properties['nome'] == piloto2:
+                        piloto2 = record['m']._properties
+
+                print('Esses são os carros cadastrados:')
+                data = []
+                for record in carros:
+                    data.append(record['c']._properties['modelo'])
+
+                for record in data:
+                    pp(record)
+
+                carro1 = input(
+                    'Qual deles deverá ser o carro do piloto n° 1? ')
+                for record in carros:
+                    if record['c']._properties['modelo'] == carro1:
+                        carro1 = record['c']._properties
+
+                carro2 = input('E o carro do piloto n° 2? ')
+                for record in carros:
+                    if record['c']._properties['modelo'] == carro2:
+                        carro2 = record['c']._properties
+
+                pb1 = probabilidadeDeVencer(piloto1, carro1)
+                pb2 = probabilidadeDeVencer(piloto2, carro2)
+
+                print('A corrida foi bem disputada, mas por habilidade (ou calvicie)')
+                if(pb1 > pb2):
+                    print('O piloto', piloto1['nome'], 'venceu!')
+                elif (pb2 > pb1):
+                    print('O piloto', piloto2['nome'], 'venceu!')
+                else:
+                    print('Os dois pilotos empataram e isso é quase impossível!!')
+                print(pb1, pb2)
+            else:
+                print(
+                    'Porém não há pilotos ou carros cadastrados :(')
+
+            divider()
         else:
             break
